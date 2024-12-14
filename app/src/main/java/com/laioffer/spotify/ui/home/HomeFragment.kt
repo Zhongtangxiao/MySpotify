4a1cbb933b2802436e9dfed78434976e6f7f18fb
oscarxiao <oscarxzt@bu.edu>
2024-12-14 14:05:08 -0500

    Adding Home Screen

--- app/src/main/java/com/laioffer/spotify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/laioffer/spotify/ui/home/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.laioffer.spotify.R
@@ -17,7 +20,13 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme(colors = darkColors()){
+                    HomeScreen(viewModel)
+                }
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
